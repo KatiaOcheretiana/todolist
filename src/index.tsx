10d0c2176f42ffc521da233ffcb60965aa6e4163
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-04-18 19:21:16 +0300

    add homepage

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./GlobalStyle";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/todolist">
+      <App />
+    </BrowserRouter>
     <GlobalStyle />
   </React.StrictMode>
 );
