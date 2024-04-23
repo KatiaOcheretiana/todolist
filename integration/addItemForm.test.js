1347607c32982093f9863cd7f4b5090a3268de12
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-04-23 10:01:23 +0300

    completed intagration tests of storybook

--- integration/addItemForm.test.js
+++ integration/addItemForm.test.js
@@ -1,7 +1,34 @@
 describe("addItemForm", () => {
-  it("base example, visually looks correct", async () => {
+  it("base addItemForm example, visually looks correct", async () => {
     await page.goto(
       "http://localhost:6006/iframe.html?id=additemform-component--add-item-form-base-example"
+    );
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it("base App example, visually looks correct", async () => {
+    await page.goto(
+      "http://localhost:6006/iframe.html?id=app-component--app-base-example"
+    );
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it("base EditableSpan example, visually looks correct", async () => {
+    await page.goto(
+      "http://localhost:6006/iframe.html?id=editablespan-component--editable-span-base-example"
+    );
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it("base TaskItem example, visually looks correct", async () => {
+    await page.goto(
+      "http://localhost:6006/iframe.html?id=taskitem-component--task-item-base-example"
     );
     const image = await page.screenshot();
 
